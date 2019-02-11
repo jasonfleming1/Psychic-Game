516d9dd881179d51817bd7771498e3ed31a7eb4a
phantom <phantomdesign9@gmail.com>
2019-02-10 20:09:49 -0600

    gamepush

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,87 +1,56 @@
-//tscooby=doo word guess
+//psychic game
 
-//the game requirements
+//game variables
 
-    //1. select a random word for the player
-    //2. take a guess from the player
-    //3. validate guess is right and replace _ in index of word.length
-    //4. record bad guess values and decrement remaining attempts
-    //5. limit players guesses to a number of tries
-    //6. event for winning
-    //7. event for failing
-    //2-5 is a loop
-    //1, 6, 7 is a singluar event that repeats after restarting game or getting a new word
+var letterArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var wins = 0; //total wins
+var losses = 0; //total losses
+var chances = 9 //max chances
+var guessed = [] // letter choses
 
-//start the game, probably with an onclick.function() {}
+//write vars to html
+document.getElementById("chances").innerHTML = chances;
+document.getElementById("countLosses").innerHTML = losses;
+document.getElementById("countWins").innerHTML = wins;
 
-//wihtin this function
+//start game trigger
 
-    //define word variables
+document.onkeyup = function (event) {
+    //found this one online, think we talked about it in brief in class to recognize characters
+    var playerGuess = event.key;
+    //defining a variable and getting a random  letter
+    var computerGuess = letterArray[Math.floor(Math.random() * letterArray.length)];
+    //creating a guessed array index
+    guessed.push(" " + playerGuess);
+    document.getElementById("guesses").innerHTML = guessed;
 
-    var wordList = ["scooby", "fred", "daphne", "velma", "shaggy"];
-    var word = []; //the random word
-    var rightGuess = []; // matching guess values
-    var wrongGuess = []; // bad guess values
-    var counterChances; // remaining chances
-    var counterRight; // number of correct entries
-    var guess; //value of user's guess
-    var chances = 10; // maximum attmptes to know word
-    var wins // win counter - number of guessed words
+    //winner
 
+    if (playerGuess == computerGuess) {
+        alert("You are a winner! Click here to play again");
+        wins++; //increment win var
+        chances = 9; //reset the chances for next game
+        guessed.length = 0; //clear guessed tracker
+        document.getElementById("chances").innerHTML = chances;
+        document.getElementById("countLosses").innerHTML = losses;
+        document.getElementById("countWins").innerHTML = wins;
+        document.getElementById("guesses").innerHTML = guessed;
 
-    //generate a random word
-
-    function getWord() {
-        word = wordList[Math.floor(Math.random() * wordList.length)];
     }
 
-    // geneate the mysteryWord conent
-
-    var mysteryWord = [];
-
-    function getArray() {
-        for (i = 0; i < word.length; i++); {
-            mysteryWord[i] = "_";
-        }
+    else if (chances == 0) {
+        alert("You lose! Click here to play again");
+        losses++; //increment loser var
+        chances = 9; //reset the chances for next game
+        guessed.length = 0; //clear guessed tracker
+        document.getElementById("guesses").innerHTML = guessed;
+        document.getElementById("chances").innerHTML = chances;
+        document.getElementById("countLosses").innerHTML = losses;
+        document.getElementById("countWins").innerHTML = wins;
     }
 
-    // track how many letters are left in the word
-
-    var remainingLetters = word.length;
-
-    // push variables to html displays
-
-        //document.getElementId(classname).innerhtml = var; (repeat for vars)
-
-    // game code
-    
-    // if var.counterChances < 10 {}
-        //THENDO onkeyup(event) 
-            //if {}
-                //var.guess isin var.rightGuess OR var.wrongGuess
-                    //alert.("you've already guessed this letter!")
-                    //return to var.counterChances <10 -- end if
-            //else if {}
-                //var.guess isIn var.mysteryWord;
-                    //charAt var.word, replace underscore with var.guess value in index
-                    //document.getElementByid(mysteryWord).innerhtml(i);
-                    //increment var.counterRight,
-                            //if var.counterRight = mysteryWord.length
-                                //then fire WIN events -- end (show restart button?)
-                            //return to var.counterChances <10 -- end else if --
-            //else {}
-                //var.guess isNotIn var.mysteryWord;
-                    //then decrement var.counterChances
-                        //if var.counterChances >= var.counterChaces 
-                            //then fire loser events -- end (show restart button?)
-                        //return to var.counterChances < 10 -- end else --
-//else
-        //THENDO fire loser events
-        //present restart button
-
-// logs and checks
-
-    console.log("var word " + word);
-    console.log("var wordList " + wordList);
-    console.log("var remainingLetters " + remainingLetters);
-    console.log("var mysterWord " + mysteryWord)
+    else {
+        chances--;
+        document.getElementById("chances").innerHTML = chances;
+    }
+}
